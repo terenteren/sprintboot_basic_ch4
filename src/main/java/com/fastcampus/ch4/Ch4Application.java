777549eb26ed7adfeaa1f2e8f9e098be77f136ac
terenteren <jaewon8752@gmail.com>
2024-03-08 17:51:03 +0900

    엔티티매니저 생성 User 엔티티 생성 엔티티 매니저로 CRUD 테스트

--- src/main/java/com/fastcampus/ch4/Ch4Application.java
+++ src/main/java/com/fastcampus/ch4/Ch4Application.java
@@ -1,6 +1,8 @@
 package com.fastcampus.ch4;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +10,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.swing.*;
+import java.util.Date;
 
 @SpringBootApplication
 public class Ch4Application implements CommandLineRunner {
@@ -25,6 +28,36 @@ public class Ch4Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("emf = " + emf);
+		EntityManager em = emf.createEntityManager();
+		System.out.println("em = " + em);
+		EntityTransaction tx = em.getTransaction();
+
+		User user = new User();
+		user.setId("aaa");
+		user.setPassword("1234");
+		user.setName("Lee");
+		user.setEmail("aaa@aaa.com");
+		user.setIn_date(new Date());
+		user.setUp_date(new Date());
+
+		tx.begin();
+		// 1. 저장
+		em.persist(user); // user엔티티를 em에 영속화(저장)
+		em.persist(user); // 같은 user엔티티를 여러번 저장해도 한번만 insert
+		// 2. 변경
+		user.setPassword("4321"); // PersistenceContext가 변경감지해 update
+		user.setEmail("bbb@bbb.com");
+		tx.commit();
+
+		// 3. 조회
+		User user2 = em.find(User.class, "aaa"); // em에 있으면 db조회 안함
+		System.out.println("(user == user2) = " + (user == user2));
+		User user3 = em.find(User.class, "bbb"); // em에 없으면 db 조회
+		System.out.println("user3 = " + user3);
+
+		// 4. 삭제
+		tx.begin();
+		em.remove(user); // em에서 user엔티티 삭제
+		tx.commit();
 	}
 }
---
+++ src/main/java/com/fastcampus/ch4/User.java
@@ -0,0 +1,77 @@
+package com.fastcampus.ch4;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class User {
+    @Id // pk 지정
+    private String id;
+    private String password;
+    private String name;
+    private String email;
+    private Date in_date;
+    private Date up_date;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", in_date=" + in_date +
+                ", up_date=" + up_date +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getIn_date() {
+        return in_date;
+    }
+
+    public void setIn_date(Date in_date) {
+        this.in_date = in_date;
+    }
+
+    public Date getUp_date() {
+        return up_date;
+    }
+
+    public void setUp_date(Date up_date) {
+        this.up_date = up_date;
+    }
+}
